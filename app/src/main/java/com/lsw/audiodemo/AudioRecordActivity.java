b43538587f05f5e3285b31d3dbf726da316f3d62
sweeneyliu <sweeneyliu@tencent.com>
2019-11-08 11:53:20 +0800

    Initial commit

---
+++ app/src/main/java/com/lsw/audiodemo/AudioRecordActivity.java
@@ -0,0 +1,123 @@
+package com.lsw.audiodemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Build;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AudioRecordActivity extends AppCompatActivity
+        implements View.OnClickListener {
+    private String TAG = "AudioRecordActivity";
+    private Button btn_start, btn_stop, btn_play, btn_onplay;
+    private AudioRecordManager mManager;
+    //申请权限列表
+    private int REQUEST_CODE = 1001;
+    private String[] permissions = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+    };
+    //拒绝权限列表
+    private List<String> refusePermissions = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_record);
+        AudioRecordManager.init();//初始化目录
+        initView();
+        checkPermission();
+    }
+
+    /**
+     * 6.0以上要动态申请权限
+     */
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (ContextCompat.checkSelfPermission(this,
+                        permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                    refusePermissions.add(permissions[i]);
+                }
+            }
+            if (!refusePermissions.isEmpty()) {
+                String[] permissions = refusePermissions.toArray(new String[refusePermissions.size()]);
+                ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE);
+            }
+        }
+
+    }
+
+    /**
+     * 权限结果回调
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults != null) {
+                for(int i = 0 ; i<permissions.length;i++){
+                    if(grantResults[i]!=PackageManager.PERMISSION_GRANTED){
+                        Log.d(TAG,permissions[i]+"   被禁用");
+                    }
+                }
+            }
+        }
+    }
+
+    private void initView() {
+        btn_start = findViewById(R.id.record_start);
+        btn_stop = findViewById(R.id.record_stop);
+        btn_play = findViewById(R.id.record_play);
+        btn_onplay = findViewById(R.id.record_noplay);
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+        btn_play.setOnClickListener(this);
+        btn_onplay.setOnClickListener(this);
+        //初始化
+        mManager = AudioRecordManager.NewInstance();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.record_start:
+                //录音
+                mManager.startRecord();
+                break;
+            case R.id.record_stop:
+                //停止
+                mManager.stopRecord();
+                break;
+            case R.id.record_play:
+                //播放
+                mManager.playRecord();
+                break;
+            case R.id.record_noplay:
+                //停止
+                mManager.stopPlayRecord();
+                break;
+            default:
+                break;
+        }
+    }
+}
